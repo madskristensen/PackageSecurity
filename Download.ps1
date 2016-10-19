ff5c7bc79989cf571ce5c286e4d5210e0c022d24
Mads Kristensen <post@madskristensen.net>
2016-10-19 09:20:31 -0700

    Updated build script

--- Download.ps1
+++ Download.ps1
@@ -1,4 +1,8 @@
 $url = "https://raw.githubusercontent.com/RetireJS/retire.js/master/repository/npmrepository.json"
 $file = Resolve-Path ".\src\Resources\npmrepository.json"
 
+Write-Host "Downloading npmrepository.json..." -ForegroundColor Cyan -NoNewline
+
 Invoke-WebRequest $url -OutFile $file
+
+Write-Host "OK" -ForegroundColor Green
